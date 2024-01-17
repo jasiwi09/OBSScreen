8f71c724532c48f5d89de3175de4ba71138582c3
Jasiwi09 <jasiwi09@gmail.com>
2024-01-17 23:53:31 +0100

    New interface and fuctions

--- main.py
+++ main.py
@@ -1,44 +1,79 @@
+#Importamos las librerías necesarias
 import pygetwindow as gw
 import time
 from obswebsocket import obsws, requests
+from dotenv import load_dotenv
+import os
+from flask import Flask, render_template, request, url_for, redirect
+import json
+import threading
 
+#Cargamos dotenv para obtener los dados sensibles desde .env
+load_dotenv()
 
-mi_diccionario = {
-    "chrome": "Time",
-    "fivem": "GTA2"
-}
-
-
-host = "192.168.0.160"
-port = 4455
-password = "RDLcLWfvhAvEREyg"
-nombre_de_la_escena = None
-
+#Nos conectamos al obswebsocket
+host = os.getenv("IP")
+port = os.getenv("PORT")
+password = os.getenv("PASS")
 ws = obsws(host, port, password)
 ws.connect()
 
-while True:
-    active_window = gw.getActiveWindow()
-    if active_window:
-        title = active_window.title.lower()
-        print('Title: ', title)
-        try:
-            if "chrome" in title:
-                nombre_de_la_escena = "Time"
-                print("Estás en Google Chrome")
-            elif "fivem" in title:
-                nombre_de_la_escena = "GTA2"
-                print("Estás en FiveM")
-            else:
-                print("No estás en Chrome ni en VS Code")
-            ws.call(requests.SetCurrentProgramScene(sceneName=nombre_de_la_escena))
-            print(f"Cambiado a la escena: {nombre_de_la_escena}")
-        except Exception as e:
-            print(f"Error: {e}")
-    else:
-        print("No hay ventana activa")
+#Lista de variables
+scene_name = None
+refresh_time = 0.1
 
-    time.sleep(1)
+jsonFileName = 'apps.json'
+
+f = open(jsonFileName)
+
+kv_array = json.load(f)
+
+f.close()
+
+#Flask APP
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global array
+    global kv_array
+    if request.method == 'POST':
+        new_app = request.form['new_app']
+        new_scene = request.form['new_scene']
+        kv_array[new_app] = new_scene
+        f = open(jsonFileName, 'w')
+        json.dump(kv_array, f)
+        f.close()
+        return redirect(url_for('index'))
+    return render_template('index.html', valor=kv_array)
+
+
+
+def run_obs_loop():
+    while True:
+        active_window = gw.getActiveWindow()
+        if active_window:
+            title = active_window.title.lower()
+            try:
+                for clave, valor in kv_array.items():
+                    if clave in title:
+                        scene_name = valor
+                        ws.call(requests.SetCurrentProgramScene(sceneName=scene_name))
+            except Exception as e:
+                print(f"Error {e}")
+        else:
+            print("No hay ventana activa")
+        time.sleep(refresh_time)
+
+    
+obs_thread = threading.Thread(target=run_obs_loop)
+obs_thread.daemon = True
+obs_thread.start()
+
+app.run()
+
+
+
 
 
 
